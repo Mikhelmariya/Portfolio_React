bdd14d6a0745f09d5ceff04cd08eee1250c3972b
Mikhelmariya <mikhela65@gmail.com>
2023-03-17 23:24:44 +0530

    navbar,home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-
+//Root component, initially run
 import './App.css';
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
+  const title ="Hey!!  I'm Mikhel V Kuttickal"
+  const myquote ="I can do all things through Christ who strengthens me"
+
   return (
     <div className="App">
+      <Navbar/>
       <div className='content'>
-        <h1>Hey!!  I'm Mikhel V Kuttickal</h1>
+        <h1>{title}</h1>
+        <p>{myquote}</p>
+        <a href="https://github.com/Mikhelmariya">Github</a>
       </div>
     </div>
   );
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+//sfc and press entre to create a stateless functional component
+const  Navbar= () => {
+    return (  
+     <nav className="navbar">
+        <h1>My portfolio</h1>
+        <div>
+            <a href="/">Home</a>
+        </div>
+     </nav>
+    );
+}
+ 
+export default Navbar;
